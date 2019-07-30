21fb20b27d33328d41dd68c1b8118400eb4853aa
Don Gepulango <dongepulango89@gmail.com>
2019-07-30 15:06:25 +0800

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/dongepulango/Documents/Works/BalkanBrothers/gatsby-kit/node_modules/gatsby-plugin-page-transitions/gatsby-browser.js'),
-      options: {"plugins":[],"transitionTime":300},
-    },{
-      plugin: require('/Users/dongepulango/Documents/Works/BalkanBrothers/gatsby-kit/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]

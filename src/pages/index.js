4496a67e4ee6aafd69577ff5bad21d62e6dc2343
Don Gepulango <dongepulango89@gmail.com>
2019-07-30 14:48:44 +0800

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 //gatsby
 import { useStaticQuery, graphql } from 'gatsby';
-
 //styles
 import styled from 'styled-components';
 import vars from '../components/varss';
@@ -13,6 +12,8 @@ import Layout from '../components/layout';
 import Container from '../components/container';
 import Heading from '../components/heading';
 import Image from '../components/image';
+//grid
+import { Row, Col } from 'styled-bootstrap-grid';
 
 //styled
 const HomeWrap = styled.section`
@@ -51,9 +52,16 @@ const Home = () => {
       <PageTransition>
         <HomeWrap>
           <Container>
-            <Heading heading1>Welcome</Heading>
-            <Image fluid={data.Image2.childImageSharp.fluid} center />
-            <Image fluid={data.Image1.childImageSharp.fluid} center />
+            <Row>
+              <Col md={6}>
+                <Heading heading1>Welcome</Heading>
+                <Image fluid={data.Image2.childImageSharp.fluid} center />
+                <Image fluid={data.Image1.childImageSharp.fluid} center />
+              </Col>
+              <Col md={6}>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas eum, enim perspiciatis magni dolorem eos ad, rem excepturi cumque voluptatum, autem sapiente laborum commodi non. Voluptas dolores et nemo ea dignissimos tempore nesciunt nostrum, commodi odit minima earum possimus voluptates illum enim asperiores omnis nulla atque ut pariatur nisi, voluptate labore fugit placeat? Ipsam, tenetur quae iusto, cum molestiae fuga, quisquam rem nostrum facilis suscipit animi. Aperiam voluptates eveniet ullam hic repellendus, ipsum accusantium maxime consectetur voluptatem animi numquam porro fugit expedita ipsam qui velit odit minima commodi! Animi blanditiis aliquid magni neque doloremque fugiat pariatur fugit eos alias aut? Eligendi.</p>
+              </Col>
+            </Row>
           </Container>
         </HomeWrap>
       </PageTransition>
